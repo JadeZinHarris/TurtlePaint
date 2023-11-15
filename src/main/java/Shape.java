284543f8bcb0bb216e00392a5c1dde4b44022caa
Jada Harris <107131999+JadeZinHarris@users.noreply.github.com>
2023-11-15 17:04:12 -0500

    Fixing Main

--- src/main/java/Shape.java
+++ src/main/java/Shape.java
@@ -1,2 +1,9 @@
+import java.awt.geom.Line2D;
+
 public abstract class Shape {
+
+    // https://www.developer.com/guides/java-2d-graphics-the-shape-interface-part-1/ axes
+
+    Turtle.draw(new Line2D.Double(-1.5*ds,0.0,1.5*ds,0.0))
+    Turtle.draw(new Line2D.Double(0.0,-1.5*ds,0.0,1.5*ds))
 }
