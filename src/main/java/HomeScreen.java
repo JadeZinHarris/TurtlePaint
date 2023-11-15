106adc1385a56684bd07b1fc726547cf9b60af5b
Jada Harris <107131999+JadeZinHarris@users.noreply.github.com>
2023-11-15 15:55:53 -0500

    adding switch cases for adding a shape and saving

--- src/main/java/HomeScreen.java
+++ src/main/java/HomeScreen.java
@@ -27,10 +27,51 @@ public class HomeScreen {
             }
 
         }
+
     }
-//    // public void addShape() {
-//        Scanner scanner = new Scanner(System.in);
-//
-//        }
+         public void addAShape() {
+        Scanner scanner = new Scanner(System.in);
+        boolean adding = true;
+        while (adding) = {
+                System.out.println("Choose a shape: ");
+                System.out.println("1.) Square");
+                System.out.println("2.) Circle");
+                System.out.println("3. Triangle");
+                String input = scanner.nextLine().trim();
+
+                switch (input) {
+                    case "1":
+                        square();
+                    case "2":
+                        circle();
+                    case "3":
+                        triangle();
+
+
+                }
+             }
+
+        }
+        public void saveAllShapes() {
+            Scanner scanner = new Scanner(System.in);
+            boolean saving = true;
+            while (saving) = {
+                System.out.println("Choose to save or delete: ");
+                System.out.println("1.) Save");
+                System.out.println("2.) Delete");
+
+                String input = scanner.nextLine().trim();
+
+                switch (input) {
+                    case "1":
+                        save();
+                    case "2":
+                        delete();
+
+
+                }
+            }
+
+        }
     }
-}
+
